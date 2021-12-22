7fc8fb4de00c1e02324656fa6fbb344262e2a84f
lishu <lishugupta652@gmail.com>
2021-12-22 23:57:34 +0530

    Canvas

--- src/components/homePage/HomeBanner.js
+++ src/components/homePage/HomeBanner.js
@@ -19,19 +19,53 @@ const HomeBanner = () => {
   console.log(size)
   useEffect(() => {
     let renderingElement = canvas.current
+    // create an offscreen canvas only for the drawings
     let drawingElement = renderingElement.cloneNode()
-
-    let renderingCtx = renderingElement.getContext("2d")
     let drawingCtx = drawingElement.getContext("2d")
-
+    let renderingCtx = renderingElement.getContext("2d")
     let lastX
     let lastY
-
     let moving = false
 
     renderingCtx.globalCompositeOperation = "source-over"
     renderingCtx.fillStyle = currentTheme === "dark" ? "#000000" : "#ffffff"
     renderingCtx.fillRect(0, 0, size.width, size.height)
+
+    renderingElement.addEventListener("mouseover", ev => {
+      moving = true
+      lastX = ev.pageX - renderingElement.offsetLeft
+      lastY = ev.pageY - renderingElement.offsetTop
+    })
+
+    renderingElement.addEventListener("click", ev => {
+      moving = true
+      lastX = ev.pageX - renderingElement.offsetLeft
+      lastY = ev.pageY - renderingElement.offsetTop
+    })
+
+    renderingElement.addEventListener("mouseup", ev => {
+      moving = false
+      lastX = ev.pageX - renderingElement.offsetLeft
+      lastY = ev.pageY - renderingElement.offsetTop
+    })
+
+    renderingElement.addEventListener("mousemove", ev => {
+      if (moving) {
+        drawingCtx.globalCompositeOperation = "source-over"
+        renderingCtx.globalCompositeOperation = "destination-out"
+        let currentX = ev.pageX - renderingElement.offsetLeft
+        let currentY = ev.pageY - renderingElement.offsetTop
+        drawingCtx.lineJoin = "round"
+        drawingCtx.moveTo(lastX, lastY)
+        drawingCtx.lineTo(currentX, currentY)
+        drawingCtx.closePath()
+        drawingCtx.lineWidth = 120
+        drawingCtx.stroke()
+        lastX = currentX
+        lastY = currentY
+        renderingCtx.drawImage(drawingElement, 0, 0)
+      }
+    })
   }, [currentTheme])
 
   return (
