653ca8c26c6b54143f8c09e52d4660f691c237dd
lishu <lishugupta652@gmail.com>
2021-12-22 14:35:57 +0530

    Layout page > Header > hammenu

--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -27,4 +27,19 @@ export const Logo = styled.nav`
     bottom: 2px;
   }
 `
-export const Menu = styled.nav``
+export const Menu = styled.nav`
+  button {
+    tranform-origin: center;
+    border: none;
+    padding: 20px;
+    background: none;
+    outline: none;
+    span {
+      width: 36px;
+      height: 6px;
+      display: block;
+      background: ${props => props.theme.text};
+      margin: 8px;
+    }
+  }
+`
