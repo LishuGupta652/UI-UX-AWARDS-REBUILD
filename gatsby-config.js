719a96d934ff833bdcb206a4a50519dbd5cf72b1
lishu <lishugupta652@gmail.com>
2021-12-22 14:11:03 +0530

    Layout page > Global Style

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `UX/UI`,
+    description: `Something Amazing is About to come.`,
+    author: `@lishugupta652`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,31 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+/// Styled Components
+import { createGlobalStyle } from "styled-components"
+import { Normalize } from "styled-normalize"
+
+const GlobalStyle = createGlobalStyle`
+${Normalize}
+  * {
+    text-decoration: none;
+    // curson: none;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body{
+    font-family: Roboto,sans-serif;
+    background:  #fff;
+    overscroll-behavior: none;
+    overflow-x: none;
+  }
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +38,12 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  return (
+    <>
+      <GlobalStyle />
+      <main>{children}</main>
+    </>
+  )
 }
 
 Layout.propTypes = {
