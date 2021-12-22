30a327c368bea4178605632570d470b972bab691
lishu <lishugupta652@gmail.com>
2021-12-22 14:18:34 +0530

    Layout page > Header styledComponents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from "react"
+import { HeaderNav } from "../styles/headerStyles"
 
 const Header = () => {
-  return <div>header</div>
+  return (
+    <HeaderNav>
+      <Container>
+        <Flex>
+          <Logo>lishu</Logo>
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
 }
 
 export default Header
