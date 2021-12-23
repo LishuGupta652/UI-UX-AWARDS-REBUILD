7ee67ca5edc22f859144ed948d1cd280bcb40f93
lishu <lishugupta652@gmail.com>
2021-12-23 18:24:14 +0530

    Reset

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from "react"
 import HomeBanner from "../components/homePage/HomeBanner"
 import Layout from "../components/layout"
+import { useGlobalDispatch, useGlobalState } from "../context/globalContext"
 
 const IndexPage = props => {
+  const { currentTheme, cursorStyles } = useGlobalState()
+  const dispatch = useGlobalDispatch()
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || "false"
+
+    dispatch({ type: "CURSOR_TYPE", cursorType })
+  }
+
   return (
     <Layout>
-      <HomeBanner />
+      <HomeBanner onCursor={onCursor} />
     </Layout>
   )
 }
