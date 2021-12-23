6ee5a2425143b61a08c16d02a385cd9b0bff4af6
lishu <lishugupta652@gmail.com>
2021-12-23 19:16:59 +0530

    Navigation: Button

--- src/styles/navigationStyles.js
+++ src/styles/navigationStyles.js
@@ -12,7 +12,28 @@ export const Nav = styled.div`
   overflow: hidden;
 `
 
-export const NavHeader = styled.div``
-export const CloseNav = styled.div``
+export const NavHeader = styled.div`
+  top: 72px;
+  position: relative;
+  h2 {
+    color: ${props => props.theme.background};
+  }
+`
+export const CloseNav = styled.div`
+  button {
+    transform-origin: center;
+    border: none;
+    padding: 20px;
+    background: none;
+    outline: none;
+    span {
+      width: 36px;
+      height: 8px;
+      display: block;
+      background: ${props => props.theme.background};
+      margin: 8px;
+    }
+  }
+`
 export const NavList = styled.div``
 export const NavFooter = styled.div``
