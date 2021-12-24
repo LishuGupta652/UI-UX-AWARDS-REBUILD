b789f1c79b22b8950d84e50ecbdda6fc761343d5
lishu <lishugupta652@gmail.com>
2021-12-24 19:27:35 +0530

    Navigation Data

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,6 +9,38 @@ import {
   NavList,
 } from "../styles/navigationStyles"
 
+const navRoutes = [
+  {
+    id: 0,
+    title: "not humble",
+    path: "/not-humble",
+    video: "featured-video.mp4",
+  },
+  {
+    id: 1,
+    title: "bleeping easy",
+    path: "/bleeping-easy",
+    video: "easy.mp4",
+  },
+  {
+    id: 2,
+    title: "make it zero",
+    path: "/make-it-zero",
+    video: "make-it-zero.mp4",
+  },
+  {
+    id: 3,
+    title: "it takes an island",
+    path: "/it-takes-an-island",
+    video: "it-takes-an-island.mp4",
+  },
+  {
+    id: 4,
+    title: "50 beaches",
+    path: "/50-beaches",
+    video: "50-beaches.mp4",
+  },
+]
 const Navigation = () => {
   return (
     <Nav>
