5d6954e2b65647b8e47379cfae731a7abc561b5a
lishu <lishugupta652@gmail.com>
2021-12-26 13:40:44 +0530

    Home About

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import HomeAbout from "../components/homePage/HomeAbout"
 import HomeBanner from "../components/homePage/HomeBanner"
 import HomeContent from "../components/homePage/HomeContent"
 import HomeFeatured from "../components/homePage/HomeFeatured"
@@ -19,6 +20,7 @@ const IndexPage = props => {
       <HomeBanner onCursor={onCursor} />
       <HomeContent />
       <HomeFeatured onCursor={onCursor} />
+      <HomeAbout />
     </Layout>
   )
 }
