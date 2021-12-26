661ba32560fd25bb5684ba341afb4bbd1e0c3fc3
lishu <lishugupta652@gmail.com>
2021-12-26 12:15:17 +0530

    Home Content JSX

---
+++ src/components/homePage/HomeContent.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Container } from "../../styles/globalStyles"
+
+const HomeContent = () => {
+  return (
+    <HomeContentSection>
+      <Container>
+        <Content>
+          Great stories don’t just happen— <br /> they need to be uncovered. And
+          we dig deep to discover the great stories that lie just below the
+          surface. Dirt under our fingernails and all.
+        </Content>
+      </Container>
+    </HomeContentSection>
+  )
+}
+
+export default HomeContent
