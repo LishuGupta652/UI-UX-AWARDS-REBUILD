b82b9409f77d37912a163f8e6223c859edc46b9a
lishu <lishugupta652@gmail.com>
2021-12-26 13:12:33 +0530

    Home featured > hovered

--- src/components/homePage/HomeFeatured.js
+++ src/components/homePage/HomeFeatured.js
@@ -31,13 +31,16 @@ const HomeFeatured = ({ onCursor }) => {
               NOT <br />
               HUMBLE
               <span className="arrow">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 57">
+                <motion.svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 101 57"
+                >
                   <path
                     d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
                     fill="#FFF"
                     fillRule="evenodd"
                   ></path>
-                </svg>
+                </motion.svg>
               </span>
             </h2>
           </FeaturedContent>
