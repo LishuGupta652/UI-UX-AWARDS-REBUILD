8a7595df0f0195649320a00588ff7c78fee646ba
lishu <lishugupta652@gmail.com>
2021-12-27 14:15:18 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react"
+import { Container } from "../styles/globalStyles"
 
 const Footer = () => {
-  return <div>footer</div>
+  return (
+    <FooterNav>
+      <Container>
+        <Flex>
+          <FooterContent>
+            <p>810.98.68.534</p>
+            <p>lishugupta652@gmail.com</p>
+          </FooterContent>
+          <FooterContent>
+            <p>Indore MP</p>
+            <p>Lishu gupta : lishu.ml</p>
+          </FooterContent>
+          <FooterSocial></FooterSocial>
+        </Flex>
+      </Container>
+    </FooterNav>
+  )
 }
 
 export default Footer
