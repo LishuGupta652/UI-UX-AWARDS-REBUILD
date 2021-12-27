22bd312436d98eb6f18caedcea6166f48c99d903
lishu <lishugupta652@gmail.com>
2021-12-27 12:40:11 +0530

    element position

---
+++ src/context/useElementPosition.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export default function useElementPosition(el) {
+  function getElement(x, y) {
+    return {
+      x,
+      y,
+    }
+  }
+
+  const [elementPosition, setElementPosition] = useState(getElement())
+
+  useEffect(() => {
+    function handlePosition() {
+      let element = el.current
+      let x =
+        element.getBoundingClientRect().left +
+        document.documentElement.scrollLeft +
+        element.offsetWidth / 2
+      let y =
+        element.getBoundingClientRect().top +
+        document.documentElement.scrollLeft +
+        element.offsetWidth / 2
+    }
+  })
+}
